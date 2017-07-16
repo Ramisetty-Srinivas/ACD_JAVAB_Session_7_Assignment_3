c9fc6a3dab4ef55cf35e1b3787382acbc19d926a
Ramisetty-Srinivas <vaasu619@gmail.com>
2017-07-16 12:11:56 +0530

    ACD_JAVAB_Session_7_Assignment_3

---
+++ src/session7_assignment3/Capacity.java
@@ -0,0 +1,22 @@
+package session7_assignment3;
+
+/*
+ * Author @ramisetty-srinivas
+ * This program helps to find how the capacity changes in StringBuffer
+ */
+
+public class Capacity {
+	
+	public static void main(String args[]){  
+		
+		StringBuffer sb=new StringBuffer();  
+		
+		System.out.println(sb.capacity());	//default 16  
+		sb.append("Hello");  
+		
+		System.out.println(sb.capacity());	//now 16  
+		sb.append("java is my favourite language");  
+		
+		System.out.println(sb.capacity());	//now (16*2)+2=34 i.e (oldcapacity*2)+2  
+	}  
+}  
